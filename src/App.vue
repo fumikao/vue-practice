65f68bae8cb222b7323a1303b1e72cf62e5e2a27
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-11 17:05:04 +0900

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <h1>Vue.jsの練習作品集</h1>
     <router-link to="/">Home</router-link>
-    <router-view/>
+    <transition name="fade">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -20,6 +22,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 50px;
+}
+
+.fade-enter-active {
+  transition: opacity 0.2s ease-in
+  }
+.fade-leave-active {
+  transition: opacity .0.2s ease-out
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
 }
 
 li {
