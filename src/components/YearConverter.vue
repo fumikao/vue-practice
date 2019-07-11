3e05e5d65cb544384a9dc8f8182632f527929db9
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-11 15:31:49 +0900

    Create year-converter

---
+++ src/components/YearConverter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h2>西暦・和暦 変換システム</h2>
+      <div>
+        <p><input type="number" v-model="year" name="year" id="year">年は...</p>
+        <table>
+          <tr>
+            <th style="width: 5em;">明治</th>
+            <td>{{ year - 1867 }} 年</td>  
+          </tr>
+          <tr>
+            <th>大正</th>
+            <td>{{ year - 1911 }} 年</td>  
+          </tr>
+          <tr>
+            <th>昭和</th>
+            <td>{{ year - 1925 }} 年</td>  
+          </tr>
+          <tr>
+            <th>平成</th>
+            <td>{{ year - 1988 }} 年</td>  
+          </tr>
+          <tr>
+            <th>令和</th>
+            <td>{{ year - 2018 }} 年</td>  
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
